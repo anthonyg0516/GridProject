a0fe50ec9095a25bf426b0e0f6fddf8d3b87042d
Anthony Griffith <griffithanthony42@gmail.com>
2025-01-29 12:41:53 -0500

    Updated source code

---
+++ ExcelimportCode.R
@@ -0,0 +1,3 @@
+TotalScore <- read_excel("Immaculate Grid.xlsx", 
+                         sheet = "Total Score", col_types = c("date", 
+                                                              "numeric"))
--- Graphs.R
+++ Graphs.R
@@ -54,6 +54,38 @@ ggplot(NewScoreDist2, aes(x = AverageScore,
   xlab("Average Team Grid Score")+
   ylab("Teams")
 
+#Average team scores, distance, appearances CHANGES
+ggplot(NewScoreDist2, aes(x = DistanceNum,
+                          y = reorder(Team, DistanceNum),
+                          color = AverageScore,
+                          shape = Appearance))+
+  geom_point(size = 4)+
+  scale_shape_manual(values = c(15, 16, 17, 18))+
+  scale_color_gradient(low = "black", high = "#5dade2")+
+  labs(shape = "# of Appearances", color = "Average Grid Score")+
+  theme(axis.text.x = element_text(angle = 0),
+        legend.position = "right",
+        panel.background = element_rect(fill = "lightgrey",
+                                        color = "lightgrey"),
+        plot.caption = element_text(hjust = 0.5))+
+  labs(title = "Average Grid Score for Each MLB Team in July 2023",
+       subtitle = "Comparing Distance and Average Team Score",
+       caption = "We are going to dive deeper into the 10 teams with the highest average grid score.")+
+  xlab("Distance (in miles)")+
+  ylab("Teams")
+
+#High scoring teams graph
+ggplot(TeamAvgDAW, aes(x = Wins,
+                       y = reorder(Team, Wins),
+                       color = AverageScore))+
+  geom_point(size = 10)+
+  theme(axis.text.x = element_text(angle = 0),
+        legend.position = "right")+
+  ggtitle("More Team Wins Lead to Lower Average Grid Scores",
+          subtitle = "My 10 Worst Average Team Scores")+
+  ylab("Teams")+
+  xlab("Team Wins through July 2023")
+
 #Average category scores, appearances
 ggplot(ScoreCatNew, aes(x = AverageScore,
                            y = reorder(Milestone, AverageScore),
@@ -72,3 +104,33 @@ ggplot(ScoreCatNew, aes(x = AverageScore,
   xlab("Average Score")+
   ylab("Milestone") 
 
+#Average category scores, appearances CHANGES
+ggplot(ScoreCatNew, aes(x = AverageScore,
+                        y = reorder(Milestone, AverageScore),
+                        color = Category,
+                        shape = Appearance))+
+  geom_point(size = 4)+
+  scale_shape_manual(values = c(15, 16, 17, 18))+
+  theme(legend.position = "bottom",
+        plot.caption = element_text(hjust = 0.5))+
+  labs(title = "Average Score for Each Milestone",
+       subtitle = "With Relation to Milestone Category
+      and Number of Appearances",
+       caption = "We're going to focus on the five highest
+       statistic categories and the two
+       highest awards categories.")+
+  xlab("Average Score")+
+  ylab("Milestone")
+
+#High score category CHANGES
+ggplot(HighScoreCat2, aes(x = AverageScore,
+                          y = reorder(Milestone, AverageScore),
+                          color = Category,
+                          shape = Appearance))+
+  geom_point(size = 10)+
+  scale_shape_manual(values = c(15, 16, 17))+
+  theme(legend.position = "right")+
+  labs(title = "More Grid Appearances Result in Lower Average Scores",
+       subtitle = "My Seven Worst Milestone Average Scores")+
+  xlab("Average Score")+
+  ylab("Milestone")
